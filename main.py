ec9e087a7ef4a7a59571959d3377285c3ba115c3
ASTRO063 <manikanta.kodimala3@gmail.com>
2021-06-03 19:19:46 +0530

    division-feature is rebased

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def addition( a= 2,b=3):
     return a+b    
+def divisionByTwo(additionValue=6):
+    return additionValue/2
 
 if __name__=="__main__":
     additionValue=addition()
+    divisionValue=divisionByTwo(additionValue)
